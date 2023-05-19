4590712d77b1722e4bba1a4ad2fa32ef526d21e2
Gabriel-Costa-Silva <gabrielcostasilva.1918@gmail.com>
2023-05-18 21:07:57 -0300

    Início do projeto

---
+++ Doncamatic/src/main/java/br/com/doncamatic/Doncamatic/DoncamaticApplication.java
@@ -0,0 +1,13 @@
+package br.com.doncamatic.Doncamatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoncamaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoncamaticApplication.class, args);
+	}
+
+}
---
+++ Doncamatic/src/test/java/br/com/doncamatic/Doncamatic/DoncamaticApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doncamatic.Doncamatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoncamaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
